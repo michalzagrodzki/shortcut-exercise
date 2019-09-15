9763b7eaba43bffe05e08d4dbe81efe2a30ed55b
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-09-15 21:15:48 +0200

    Add simple form and view element to clientside

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message: '',
+      input: ''
+    };
+
+    this.postMessage = this.postMessage.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
+  }
+
+  websocket = new WebSocket('ws://localhost:3333');
+
+  postMessage (event) {
+    event.preventDefault();
+    console.log(this.state.input)
+    this.websocket.send(JSON.stringify({
+      content: this.state.input
+    }));
+    this.setState({
+      message: [this.state.input, ...this.state.message]
+    });
+  }
+
+  handleFormChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  componentDidMount() {
+    console.log('mounting component');
+    this.websocket.onopen = () => {
+      console.log('WebSocket Client Connected');
+    }
+    this.websocket.onmessage = (message) => {
+      console.log('Getting message from server');
+      console.log(message.data);
+      this.setState({
+        message: message.data
+      });
+    }
+  }
+
+  render() {
+    const { message, input } = this.state;
+    return (
+      <div className="App">
+        <p>Message:</p>
+        <form onSubmit={this.postMessage}>
+          <input name="input" type="text" value={input} onChange={this.handleFormChange}/>
+          <button>Send</button>
+        </form>
+        <p>{ message }</p>
+      </div>
+    );
+  }
 }
 
 export default App;
