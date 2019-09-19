7a2d7234d1cb96896f00f544f42c4f16c18f9510
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-09-19 11:46:06 +0200

    Add stream for specific topic

---
+++ src/components/Topic/Topic.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import "./Topic.scss";
+
+class Topic extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      tweets: [],
+      title: '',
+      error: {
+        message: ''
+      }
+    };
+  }
+
+  websocket = new WebSocket('ws://localhost:3333/api/topic');
+
+  componentDidMount() {
+    this.setState({
+      title: 'Twitter Topic Stream',
+    });
+
+    this.websocket.onopen = () => {
+      console.log('WebSocket Client Connected');
+    }
+    this.websocket.onmessage = (data) => {
+      console.log('Getting message from server');
+      const tweet = data.data
+      console.log(tweet);
+      this.setState({
+        tweets: [ tweet, ...this.state.tweets]
+      });
+    }    
+  }
+
+  componentWillUnmount() {
+  }
+
+  render() {
+    const { title, tweets } = this.state;
+    return (
+      <div className="Topic">
+        <section>
+          <div className="topic-head-section">
+            <h1>{ title }</h1>
+          </div>
+        </section>
+        <section>
+          <div className="topic-column-section">
+            { 
+              tweets.map(tweet =>
+                <p>{ tweet.text }</p>
+              )
+            }
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Topic;
