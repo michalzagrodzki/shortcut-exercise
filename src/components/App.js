5a67c65522104dd69d47449571ef53749085c959
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-09-20 14:57:24 +0200

    Add routes to client side

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Main from './Main/Main'
+import Stream from './Stream/Stream'
+import Topic from './Topic/Topic'
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/stream" component={Stream} />
+          <Route path="/topic/:name" component={Topic} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
