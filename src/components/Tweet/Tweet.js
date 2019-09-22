ba8e84a3ab06638f8b444853c498167f317a4c3c
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-09-22 22:03:49 +0200

    Add image and user name to tweet component, style it.

--- src/components/Tweet/Tweet.js
+++ src/components/Tweet/Tweet.js
@@ -11,10 +11,22 @@ class Tweet extends React.Component {
   }
 
   render() {
+
+    let userImage;
+    let userName;
+
+    if (this.props.tweet.user.profile_image_url_https !== undefined) { userImage = <img src={ this.props.tweet.user.profile_image_url_https } alt="" />; }
+    if (this.props.tweet.user.name !== undefined) { userName = <h4>{ this.props.tweet.user.name }</h4>; }
+
+
     return (
       <div className="Tweet">
         <section>
           <div className="tweet-body-section">
+            <div className="tweet-header-section">
+              { userImage }
+              { userName }
+            </div>
             <p>{ this.props.tweet.text }</p>
           </div>
         </section>
