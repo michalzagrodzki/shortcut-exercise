813c6fd80f248309ec69af46912fe2e09ac0c9d5
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-09-22 22:55:10 +0200

    Update test for App component

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { MemoryRouter } from "react-router-dom";
+import { render, unmountComponentAtNode } from "react-dom";
 import ReactDOM from 'react-dom';
-import App from './../components/App';
+import App from './../components/App.js';
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
   ReactDOM.render(
   	<MemoryRouter>
   	  <App />
   	</MemoryRouter>,
-  	div);
-  ReactDOM.unmountComponentAtNode(div);
+  	container);
 });
